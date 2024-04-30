e7107eac5c72d7771373b60bbbaa0e02e5fd19a7
eladtpro <eladt.pro@gmail.com>
2024-04-30 11:23:51 +0300

    tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -22,3 +22,17 @@ for (var i = 0; i < TEST_ITERATIONS; i++) {
   });
 
 }
+
+test('Search playwrite', async ({ page }) => {
+  await page.goto('https://playwright.dev/docs/intro');
+
+  await page.getByLabel('Search').click();
+  await page.getByPlaceholder('Search docs').fill('Locators')
+  // await page.getByPlaceholder('Search docs').click();  
+  await page.waitForTimeout(1000);
+  await page.keyboard.press('Enter');
+
+  await page.waitForURL(/.*docs\/locators/);
+  // Expect a title "to contain" a substring.
+  await expect(page.getByRole('heading', { name: 'Locators', exact: true })).toBeVisible();
+});
